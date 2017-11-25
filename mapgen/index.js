ac4509bd61e459050b816e2077adfea3ecd5d09a
Sieger Falkena <Sieger@ATHENA>
2017-11-25 15:03:25 +0100

    refactor generate to builder pattern

--- mapgen/index.js
+++ mapgen/index.js
@@ -1,14 +1,18 @@
-var hexX = 25;
-var hexY = 25;
+//import { map } from "bluebird";
+
+var hexX = 26;
+var hexY = 26;
 var R = 15;
 var offset = 15;
 var mywidth = R * 2;
 var mydist = mywidth * (3 / 4)
 var myheight = (Math.sqrt(3) / 2) * mywidth
 
+var levels = 4;
+
 var water = [1, 20, '#2E86C1', 'water'];
 var flatland = [2, 0, '#DAF7A6', 'flatland'];
-var mountain = [3, 15, '#BA4A00', 'mountain'];
+var mountain = [3, 20, '#BA4A00', 'mountain'];
 var predist = [water, flatland, mountain]
 
 var distribution = (function () {
@@ -21,77 +25,87 @@ var distribution = (function () {
     return dist;
 })()
 
-function isvalid(level_map, x, y) {
-    return (x >= 0 && x < level_map.length) &&
-        (y >= 0 && y < level_map[0].length) ?
-        true : false;
-}
-
-function smooth(level_map, smooth_strength = 1) {
-    var smooth_level_map = new Array(level_map.length)
-    for (var lx = 0; lx < level_map.length; lx++) {
-        smooth_level_map[lx] = new Array(level_map[0].length)
-        for (var ly = 0; ly < level_map[lx].length; ly++) {
-            //get neighbours
-            /*
-            y + 1
-            y - 1
-            x + 1, y + 1
-            x - 1, y + 1
-            x + 1
-            x - 1
-            */
-            var temp = [];
-            temp.push(level_map[lx][ly])
-            if (isvalid(level_map, lx, ly + 1)) {
-                temp.push(level_map[lx][ly + 1] * smooth_strength);
+class GeographyBuilder {
+    // width
+    // height
+    // map
+    // distribution
+    constructor(width, height, distribution) {
+        this.height = height;
+        this.distribution = distribution;
+        this.width = width;
+        this.lerps = 1;
+        this.map = new Array(width)
+        for (var x = 0; x < width; x++) {
+            this.map[x] = new Array(height);
+            for (var y = 0; y < height; y++) {
+                this.map[x][y] = distribution[Math.round(Math.random() * (distribution.length - 1))];
             }
-            if (isvalid(level_map, lx, ly - 1)) {
-                temp.push(level_map[lx][ly - 1]* smooth_strength);
-            }
-            if (isvalid(level_map, lx + 1, ly + 1)) {
-                temp.push(level_map[lx + 1][ly + 1]* smooth_strength);
-            }
-            if (isvalid(level_map, lx - 1, ly + 1)) {
-                temp.push(level_map[lx - 1][ly + 1]* smooth_strength);
-            }
-            if (isvalid(level_map, lx + 1, ly)) {
-                temp.push(level_map[lx + 1][ly]* smooth_strength);
-            }
-            if (isvalid(level_map, lx - 1, ly)) {
-                temp.push(level_map[lx - 1][ly]* smooth_strength);
-            }
-            //smooth neighbours
-            var temp_total = 0;
-            temp.forEach(element => {
-                temp_total += element
-            });
-            temp_total /= 1 + ((temp.length -1) * smooth_strength);
-
-            smooth_level_map[lx][ly] = Math.round(temp_total)
         }
     }
-    return smooth_level_map
-}
 
-//flat topped; W = 2 R, H = (2/3) R
-function generateMap(xsize, ysize, maxHeight, minHeight) {
-    //flat topped
-    var hwratio = 1 / (Math.sqrt(3) / 2) * xsize;
-    var realy = ysize * hwratio;
-
-    var map = new Array(xsize);
-    for (var i = 0; i < xsize; i++) {
-        map[i] = new Array(ysize)
-        for (var j = 0; j < ysize; j++) {
-            map[i][j] = distribution[Math.round(Math.random() * (distribution.length - 1))];
-        }
+    static isvalid(x, y) {
+        return (x >= 0 && x < map.length) &&
+            (y >= 0 && y < map[0].length) ?
+            true : false;
     }
-    var pre_level = [9, 4, 6]
-    for (var levels = 3; levels > 0; levels--) {
-        var lerp = pre_level[levels - 1];
-        var lerpX = pre_level[levels - 1];
-        var lerpY = pre_level[levels - 1];
+
+    static smooth(level_map = map, smooth_strength = 0.4) {
+        var smooth_level_map = new Array(level_map.length)
+        for (var lx = 0; lx < level_map.length; lx++) {
+            smooth_level_map[lx] = new Array(level_map[0].length)
+            for (var ly = 0; ly < level_map[lx].length; ly++) {
+                //get neighbours
+                /*
+                y + 1
+                y - 1
+                x + 1, y + 1
+                x - 1, y + 1
+                x + 1
+                x - 1
+                */
+                var temp = [];
+                temp.push(level_map[lx][ly])
+                if (GeographyBuilder.isvalid(level_map, lx, ly + 1)) {
+                    temp.push(level_map[lx][ly + 1] * smooth_strength);
+                }
+                if (GeographyBuilder.isvalid(level_map, lx, ly - 1)) {
+                    temp.push(level_map[lx][ly - 1] * smooth_strength);
+                }
+                if (GeographyBuilder.isvalid(level_map, lx + 1, ly + 1)) {
+                    temp.push(level_map[lx + 1][ly + 1] * smooth_strength);
+                }
+                if (GeographyBuilder.isvalid(level_map, lx - 1, ly + 1)) {
+                    temp.push(level_map[lx - 1][ly + 1] * smooth_strength);
+                }
+                if (GeographyBuilder.isvalid(level_map, lx + 1, ly)) {
+                    temp.push(level_map[lx + 1][ly] * smooth_strength);
+                }
+                if (GeographyBuilder.isvalid(level_map, lx - 1, ly)) {
+                    temp.push(level_map[lx - 1][ly] * smooth_strength);
+                }
+                //smooth neighbours
+                var temp_total = 0;
+                temp.forEach(element => {
+                    temp_total += element
+                });
+                temp_total /= 1 + ((temp.length - 1) * smooth_strength);
+
+                smooth_level_map[lx][ly] = Math.round(temp_total)
+            }
+        }
+        return smooth_level_map
+    }
+
+    smooth(strength = 0.5){
+        this.map = GeographyBuilder.smooth(this.map,strength)
+        return this;
+    }
+
+    ///generate a random map with distribution, then interpolate to map at large
+    noise(width, height, distribution = distribution, smooth = 0) {
+        var lerpX = width;
+        var lerpY = height;
         var tempmap = new Array(lerpX);
         for (var lx = 0; lx < tempmap.length; lx++) {
             tempmap[lx] = new Array(lerpY);
@@ -99,32 +113,41 @@ function generateMap(xsize, ysize, maxHeight, minHeight) {
                 tempmap[lx][ly] = distribution[Math.round(Math.random() * (distribution.length - 1))]
             }
         }
+        if(smooth != 0){
+            GeographyBuilder.smooth(tempmap, smooth);
+        }
 
-        var ratiox = map.length / tempmap.length;
-        var ratioy = map[0].length / tempmap[0].length;
-        for (var lx = 0; lx < map.length; lx++) {
-            for (var ly = 0; ly < map[lx].length; ly++) {
-                map[lx][ly] = tempmap[Math.floor(lx / ratiox)][Math.floor(ly / ratioy)] + map[lx][ly];
+        var ratiox = this.map.length / tempmap.length;
+        var ratioy = this.map[0].length / tempmap[0].length;
+        for (var lx = 0; lx < this.map.length; lx++) {
+            for (var ly = 0; ly < this.map[lx].length; ly++) {
+                this.map[lx][ly] = tempmap[Math.floor(lx / ratiox)][Math.floor(ly / ratioy)] + this.map[lx][ly];
             }
         }
+        this.lerps++;
+        return this;
     }
 
-    for(var lx = 0 ; lx < map.length; lx++){
-        for(var ly =0 ; ly < map[0].length; ly++){
-            map[lx][ly] /= 4;
-            map[lx][ly] = Math.round(map[lx][ly]);
+    build(){
+        for (var lx = 0; lx < this.map.length; lx++) {
+            for (var ly = 0; ly < this.map[lx].length; ly++) {
+                this.map[lx][ly] = Math.round(this.map[lx][ly] / this.lerps);
+            }
         }
+        return this.map;
     }
-
-    return map;
 }
-var mapped = generateMap(25, 25, 3, 3);
-var mapped2 = smooth(smooth(mapped, 0.25), 0.25)
 
-console.log(mapped)
+
+var mapped = new GeographyBuilder(hexX,hexY,distribution)
+.noise(4,4,distribution,0.5)
+.noise(7,7,distribution,0.3)
+.noise(9,9,distribution,0.25)
+.smooth(0.25).build();
+
 
 function setup() {
-    createCanvas(1400, 700);
+    createCanvas(1800, 700);
 }
 
 function draw() {
@@ -150,8 +173,8 @@ function draw() {
 
             push();
             noStroke()
-            fill(predist[mapped2[i][j] - 1][2])
-            polygon((i * mydist) + 50 * R, (j * myheight) + reminderY + offset, R, 6);
+            fill(predist[mapped[hexX - i - 1][hexY - j - 1] - 1][2])
+            polygon((i * mydist) + 40 * R, (j * myheight) + reminderY + offset, R, 6);
             pop();
         }
     }
